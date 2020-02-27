68888b27499ce7a1edbb8b7149306d99f2969a47
JayDub21 <justin.wofford.21@gmail.com>
2020-02-27 14:30:05 -0600

    dev branch

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
@@ -21,13 +21,15 @@ app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/name of database");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/name of database"
+);
 
 // Start the API server
-app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
