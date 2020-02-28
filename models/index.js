2efaed47af4548bc27c029e564999c6a49ece8ee
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-27 21:00:43 -0600

    created a new routes folder

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 var mongoose = require(mongoose),
     User = require('./user');
 
-// var connStr = 'mongodb://localhost:27017/mongoose-bcrypt-test';
-// mongoose.connect(connStr, function(err) {
-//     if (err) throw err;
-//     console.log('Successfully connected to MongoDB');
-// });
+var connStr = 'mongodb://localhost:27017/mongoose-bcrypt-test';
+mongoose.connect(connStr, function(err) {
+    if (err) throw err;
+    console.log('Successfully connected to MongoDB');
+});
 
 // create a user a new user
 var testUser = new User({
