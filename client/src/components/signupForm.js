c5a90d5391b464dcf180a228d26d9ae117610aa9
lil L <57925562+leolopez10@users.noreply.github.com>
2020-02-27 21:59:31 -0600

    created componets for the form to begin testing sign up submit action

--- client/src/components/signupForm.js
+++ client/src/components/signupForm.js
@@ -1,6 +1,30 @@
 import React from "react";
+import API from "../utils/API";
+import {Input, FormBtn} from "./Form";
 
 function SignUpForm(){
+
+  //Setting up signup forms state
+  const [formObject, setFormObject] = useState({});
+
+  //Handles updating the component state while user is typing
+  function handleInputChange(event) {
+    const {name, value} = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+  //When the form is submitted, use the API.saveUser methode to save the user to the database
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.email && formObject.password){
+      API.saveUser({
+        email: formObject.email,
+        password: formObject.password
+      })
+      .catch(err => console.log(err));
+    }
+  }
+  
     return(
         <div className="container">
         <div className="row">
@@ -9,17 +33,17 @@ function SignUpForm(){
             <form className="signup">
               <div className="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="email-input" placeholder="Email"/>
+                <Input onChange={handleInputChange} type="email" name="email" id="email-input" placeholder="Email"/>
               </div>
               <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="password-input" placeholder="Password"/>
+                <Input onChange={handleInputChange} type="password" name="password" id="password-input" placeholder="Password"/>
               </div>
               <div style={{display: "none"}} id="alert" className="alert alert-danger" role="alert">
                 <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
                 <span className="sr-only">Error:</span> <span className="msg"></span>
               </div>
-              <button type="submit" className="btn btn-default">Sign Up</button>
+              <FormBtn type="submit" onClick={handleFormSubmit}>Sign Up</FormBtn>
             </form>
             <br />
             <p>Or log in <a to="/">here</a></p>
