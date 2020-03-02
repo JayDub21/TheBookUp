879b05baa863725f20708cf10116578c460c1d8c
Harrison Thomas <hthomas146@utexas.edu>
2020-03-02 12:49:39 -0600

    Fixed unique email requirement bug

--- models/listing.js
+++ models/listing.js
@@ -5,7 +5,7 @@ const ListingSchema = new Schema({
     author: { type: String, required: true },
     title: { type: String, required: true },
     edition: { type: String, required: false },
-    email: { type: String, trim: true, required: true, index: { unique: true }, match: [/.+@.+\..+/, "Please enter a valid e-mail address"] },
+    email: { type: String, trim: true, required: true, match: [/.+@.+\..+/, "Please enter a valid e-mail address"] },
     ISBN: { type: Number, required: true },
     price: { type: Number, required: true }
 });
