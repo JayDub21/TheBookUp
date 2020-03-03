9733ab5f0114d60849f9d38a34e72792d83a3c1a
abickford <atb5498@gmail.com>
2020-03-03 14:02:45 -0600

    user input is stored to the database after posting a listing

--- models/listing.js
+++ models/listing.js
@@ -7,7 +7,9 @@ const ListingSchema = new Schema({
     publishedDate: { type: String, required: false },
     email: { type: String, trim: true, required: true, match: [/.+@.+\..+/, "Please enter a valid e-mail address"] },
     ISBN: { type: Number, required: true },
-    image: { type: String, required: false }
+    image: { type: String, required: false },
+    condition: { type: String, required: false },
+    price: { type: String, required: false }
 });
 
 const Listing = mongoose.model('Listing', ListingSchema);
