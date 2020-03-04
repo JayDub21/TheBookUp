fb2dc3544e36493dd564dea3ade4b647ca3e176f
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-03 18:17:45 -0600

    moved user auth context to app.js for testing

--- client/src/utils/authContext.js
+++ client/src/utils/authContext.js
@@ -1,9 +1,42 @@
-import React from "react"
+import React, { createContext, useReducer, useContext } from "react"
 
-const AuthContext = React.createContext({
+const AuthContext = createContext();
+const { Provider } = AuthContext;
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN":
+            localStorage.setItem("user", JSON.stringify(action.payload.user));
+            localStorage.setItem("token", JSON.stringify(action.payload.token));
+            return {
+                ...state,
+                isAuthentiated: true,
+                user: action.payload.user,
+                token: action.payload.token
+            };
+        case "LOGOUT":
+            localStorage.clear();
+            return {
+                ...state,
+                isAuthentiated: false,
+                user: null
+            };
+        default:
+            return state;
+    }
+};
+
+const userState = {
     isAuthentiated: false,
     user: null,
     token: null
-});
+};
 
-export default AuthContext;
+const AuthProvider = () => {
+    const [state, dispatch] = useReducer(reducer, userState)
+}
+
+
+
+export default { AuthContext, reducer };
